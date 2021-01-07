bba4502860c944f640602d62fb9633ca6619fc90
kwdoyle <kwdoyle1@gmail.com>
2021-01-07 09:37:48 -0500

    finds column names to use from the file itself. can now analyze output from other projects

--- parse_cmd.py
+++ parse_cmd.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 import ast
+import numpy as np
 import pandas as pd
+from collections import Counter
 from mne.stats import fdr_correction as fdr
 
 pd.options.mode.chained_assignment = None
 
 # NOTE: can get the model output dataframe w/ cmd significance Y/N in a column per recording
 # from read_file2's output
+
+def find_column(df, namecols):
+    col_ix = np.concatenate([np.where(df.columns.str.contains(x))[0] for x in namecols])
+    countix = Counter(col_ix)
+    concensus_ix = np.where(np.array(list(countix.values())) == max(countix.values()))[0][0]
+    ix_use = list(countix.keys())[concensus_ix]
+
+    name_use = df.columns[ix_use]
+
+    return name_use
+
 
 def find_signif_files2(x, orig_dat):
     new_df = pd.DataFrame()
@@ -29,21 +42,34 @@ def read_file2(input_file):
     # wow, wtf, I didn't remove CS and MCSp recordings in this?????
     # not explicitly necessary with newer analyzed files as I don't put them in separate folders
     # per CRS-R score group.
-    model_data = model_data[~model_data['rec_name'].str.contains('/CS/')]
-    model_data = model_data[~model_data['rec_name'].str.contains('/MCSp/')]
+
+    # find recording name column to use
+    # namecols = ['rec_name', 'recname', 'rec.name', 'rec name',
+    #             'fname', 'filename', 'just_filename', 'just_fname']
+    # fname_col_ix = np.where([any(model_data.columns.str.contains(x)) for x in namecols])[0][0]
+    # fname_col = namecols[fname_col_ix]
+
+    fname_col = find_column(df=model_data, namecols=['rec_name', 'recname', 'rec.name', 'rec name',
+                                                     'fname', 'filename', 'just_filename', 'just_fname'])
+
+    model_data = model_data[~model_data[fname_col].str.contains('/CS/')]
+    model_data = model_data[~model_data[fname_col].str.contains('/MCSp/')]
     # remove rows where model failed to run (AUC == NaN or == 0)
     bad_recs = model_data[pd.isnull(model_data.AUC) | model_data.AUC == 0]
     model_data.drop(bad_recs.index, inplace=True)
-    bad_rec_names = bad_recs.rec_name.tolist()
+    # bad_rec_names = bad_recs.rec_name.tolist()
+    bad_rec_names = bad_recs[fname_col].tolist()
 
     model_data['pvalue'] = pd.to_numeric(model_data['pvalue'])
     # extract MRNs
     try:
-        model_data['mrn'] = model_data['rec_name'].str.split('_').apply(lambda x: x[0]).astype('int')
+        model_data['mrn'] = model_data[fname_col].str.split('_').apply(lambda x: x[0]).astype('int')
     except ValueError:
         # print('Getting MRNs from full file path instead..')
-        model_data['mrn'] = model_data['rec_name'].str.split('/').apply(lambda x: x[2]).str.split('_').apply(
-            lambda x: x[0]).astype('int')
+        # model_data['mrn'] = model_data['rec_name'].str.split('/').apply(lambda x: x[2]).str.split('_').apply(
+          #  lambda x: x[0]).astype('int')
+        use = model_data[fname_col].str.split('/').apply(lambda x: x[len(x) - 1])
+        model_data['mrn'] = use.str.split('_').apply(lambda x: x[0]).astype('int')
     # FDR correct to obtain significant files
     model_data_grp = model_data.groupby('mrn')
     fdr_pvals = pd.DataFrame(model_data_grp.apply(lambda x: fdr(x.pvalue))).reset_index()
@@ -58,11 +84,19 @@ def read_file2(input_file):
 
 
 def get_cmd_recs(df):
+    # namecols = ['rec_name', 'recname', 'rec.name', 'rec name',
+    #             'fname', 'filename', 'just_filename', 'just_fname']
+    # fname_col_ix = np.where([any(df.columns.str.contains(x)) for x in namecols])[0][0]
+    # fname_col = namecols[fname_col_ix]
+
+    fname_col = find_column(df, namecols=['rec_name', 'recname', 'rec.name', 'rec name',
+                                          'fname', 'filename', 'just_filename', 'just_fname'])
+
     cmd_rec_dat = df[df.p_signif == True]
     noncmd_rec_dat = df[df.p_signif == False]
 
-    cmd_dat_splt = cmd_rec_dat.rec_name.str.split('/').tolist()
-    noncmd_dat_splt = noncmd_rec_dat.rec_name.str.split('/').tolist()
+    cmd_dat_splt = cmd_rec_dat[fname_col].str.split('/').tolist()
+    noncmd_dat_splt = noncmd_rec_dat[fname_col].str.split('/').tolist()
 
     cmd_recs = [x[len(x) - 1] for x in cmd_dat_splt]
     noncmd_recs = [x[len(x) - 1] for x in noncmd_dat_splt]
@@ -71,14 +105,19 @@ def get_cmd_recs(df):
 
 
 def parse_perm_auc(df):
+    fname_col = find_column(df, namecols=['rec_name', 'recname', 'rec.name', 'rec name',
+                                           'fname', 'filename', 'just_filename', 'just_fname'])
+
+    permauc_col = find_column(df, namecols=['perm_scores', 'perm_AUCs', 'perm_AUC', 'permAUC', 'permscores', 'permAUCs'])
+
     perm_aucs = {}
     for ix, rw in df.iterrows():
-        rec = rw.rec_name
+        rec = rw[fname_col]
         # get rid of extra filepath if it exists
         if len(rec.split('/')) > 1:
             rec = rec.split('/')[len(rec.split('/')) - 1]
 
-        permaucs = ast.literal_eval(rw.perm_scores)
+        permaucs = ast.literal_eval(rw[permauc_col])
         perm_aucs[rec] = permaucs
 
     return perm_aucs
